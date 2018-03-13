a2d7a7034976812b7cf3b965c4325613ca616424
Tony Brix <tony@brix.ninja>
2018-03-13 08:10:09 -0500

    update to jasmine3

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,6 +1,4 @@
 "use babel";
-/* global atom, waitsForPromise */
-/* eslint-env jasmine */
 
 const {Range} = require("atom");
 const shell = require("shell");
@@ -9,107 +7,107 @@ const {getSuggestionForWord} = require("../lib/main.js").getProvider();
 function callSuggestion(suggestion) {
 	if (suggestion && suggestion.callback) {
 		suggestion.callback();
+	} else {
+		throw new Error("No suggestion callback");
 	}
 }
 
-function checkRange(suggestion, range) {
-	if (suggestion && suggestion.range) {
-		return suggestion.range.isEqual(range);
-	}
-	return false;
-}
+describe("hyperlink-hyperclick", function () {
+	beforeEach(async function () {
+		await atom.packages.activatePackage("language-gfm");
+		await atom.packages.activatePackage("language-hyperlink");
+		await atom.packages.activatePackage("hyperlink-hyperclick");
 
-describe("hyperlink-hyperclick", () => {
-	let textEditor;
+		this.textEditor = await atom.workspace.open("sample.md");
+		this.textEditor.setGrammar(atom.grammars.grammarForScopeName("source.gfm"));
 
-	beforeEach(() => {
-		waitsForPromise(() => {
-			return atom.packages.activatePackage("language-gfm");
+		spyOn(shell, "openExternal");
+
+		jasmine.addMatchers({
+			toHaveRange: function () {
+				return {
+					compare: function (suggestion, expected) {
+						if (!suggestion || !suggestion.range) {
+							throw new Error("No suggestion range");
+						}
+						const result = {};
+						result.pass = suggestion.range.isEqual(expected);
+						const not = (result.pass ? "not " : "");
+
+						const expectedRange = Range.fromObject(expected);
+
+						result.message = `Expected ${suggestion.range} ${not}to be equal to ${expectedRange} class`;
+						return result;
+					}
+				};
+			},
 		});
 
-		waitsForPromise(() => {
-			return atom.packages.activatePackage("language-hyperlink");
-		});
-
-		waitsForPromise(() => {
-			return atom.packages.activatePackage("hyperlink-hyperclick");
-		});
-
-		waitsForPromise(() => {
-			return atom.workspace.open("sample.md");
-		});
-
-		runs(() => {
-			textEditor = atom.workspace.getActiveTextEditor();
-			spyOn(shell, "openExternal");
-		});
 	});
 
-	it("http is valid", () => {
+	it("matches a valid http hyperlink", async function () {
+
+		await atom.packages.activatePackage("language-hyperlink");
 		atom.config.set("hyperlink-hyperclick.protocols", ["http"]);
-		textEditor.setText("<http://example.com>");
+		this.textEditor.setText("<http://example.com>");
 		const range = new Range([0, 1], [0, 2]);
-		const suggestion = getSuggestionForWord(textEditor, null, range);
+		const suggestion = getSuggestionForWord(this.textEditor, null, range);
 		callSuggestion(suggestion);
 
-		expect(checkRange(suggestion, [[0, 1], [0, 19]])).toBe(true);
+		expect(suggestion).toHaveRange([[0, 1], [0, 19]]);
 		expect(shell.openExternal).toHaveBeenCalled();
-		expect(shell.openExternal.argsForCall[0][0]).toBe("http://example.com");
+		expect(shell.openExternal.calls.mostRecent().args[0]).toBe("http://example.com");
 	});
 
-	it("http is not valid", () => {
+	it("only matches configured protocols", function () {
 		atom.config.set("hyperlink-hyperclick.protocols", ["https"]);
-		textEditor.setText("<http://example.com>");
+		this.textEditor.setText("http://example.com");
 		const range = new Range([0, 1], [0, 2]);
-		const suggestion = getSuggestionForWord(textEditor, null, range);
-		callSuggestion(suggestion);
-
-		expect(shell.openExternal).not.toHaveBeenCalled();
+		const suggestion = getSuggestionForWord(this.textEditor, null, range);
+		expect(suggestion).toBeNull();
 	});
 
-	it("any configured protocol is valid", () => {
+	it("matches any configured protocol", function () {
 		atom.config.set("hyperlink-hyperclick.protocols", ["test"]);
-		textEditor.setText("test://example.com");
+		this.textEditor.setText("test://example.com");
 		const range = new Range([0, 0], [0, 1]);
-		const suggestion = getSuggestionForWord(textEditor, null, range);
+		const suggestion = getSuggestionForWord(this.textEditor, null, range);
 		callSuggestion(suggestion);
 
-		expect(checkRange(suggestion, [[0, 0], [0, 18]])).toBe(true);
+		expect(suggestion).toHaveRange([[0, 0], [0, 18]]);
 		expect(shell.openExternal).toHaveBeenCalled();
-		expect(shell.openExternal.argsForCall[0][0]).toBe("test://example.com");
+		expect(shell.openExternal.calls.mostRecent().args[0]).toBe("test://example.com");
 	});
 
-	it("text is not valid", () => {
+	it("does not match just protocol", function () {
 		atom.config.set("hyperlink-hyperclick.protocols", ["test"]);
-		textEditor.setText("test");
+		this.textEditor.setText("test");
 		const range = new Range([0, 1], [0, 2]);
-		const suggestion = getSuggestionForWord(textEditor, null, range);
-		callSuggestion(suggestion);
-
-		expect(shell.openExternal).not.toHaveBeenCalled();
+		const suggestion = getSuggestionForWord(this.textEditor, null, range);
+		expect(suggestion).toBeNull();
 	});
 
-	it("gfm reference is valid", () => {
+	it("matches gfm link", function () {
 		atom.config.set("hyperlink-hyperclick.protocols", ["http"]);
-		textEditor.setText("[test][1]\n\n[1]: http://test.com");
+		this.textEditor.setText("[test][1]\n\n[1]: http://test.com");
 		const range = new Range([0, 7], [0, 8]);
-		const suggestion = getSuggestionForWord(textEditor, null, range);
+		const suggestion = getSuggestionForWord(this.textEditor, null, range);
 		callSuggestion(suggestion);
 
-		expect(checkRange(suggestion, [[0, 7], [0, 8]])).toBe(true);
+		expect(suggestion).toHaveRange([[0, 7], [0, 8]]);
 		expect(shell.openExternal).toHaveBeenCalled();
-		expect(shell.openExternal.argsForCall[0][0]).toBe("http://test.com");
+		expect(shell.openExternal.calls.mostRecent().args[0]).toBe("http://test.com");
 	});
 
-	it("checks for a link in the middle of the token", () => {
+	it("matches a url not at the start of a token", function () {
 		atom.config.set("hyperlink-hyperclick.protocols", ["test"]);
-		textEditor.setText("a test://example.com");
+		this.textEditor.setText("a test://example.com");
 		const range = new Range([0, 2], [0, 3]);
-		const suggestion = getSuggestionForWord(textEditor, null, range);
+		const suggestion = getSuggestionForWord(this.textEditor, null, range);
 		callSuggestion(suggestion);
 
-		expect(checkRange(suggestion, [[0, 2], [0, 20]])).toBe(true);
+		expect(suggestion).toHaveRange([[0, 2], [0, 20]]);
 		expect(shell.openExternal).toHaveBeenCalled();
-		expect(shell.openExternal.argsForCall[0][0]).toBe("test://example.com");
+		expect(shell.openExternal.calls.mostRecent().args[0]).toBe("test://example.com");
 	});
 });
